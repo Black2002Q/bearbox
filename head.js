062e27dd9078dcedca0e0cde78a4b4e3160f5eba
DevMB <168116018+Black2002Q@users.noreply.github.com>
2025-06-12 13:08:17 -0500

    Update head.js

--- head.js
+++ head.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
         <link rel="stylesheet" href="style.css">
-        <link rel="shortcut icon" href="bear_logo.png" type="image/x-icon">
+        <link rel="shortcut icon" href="/bear_logo.png" type="image/x-icon">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     
         <title>BearBox23 X-Fit</title>
